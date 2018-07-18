f7d58b871b5943ed1400b0f60b1a56447b742a87
Puyan Wei <puyanwei@gmail.com>
2018-07-18 16:25:39 +0100

    Create main page, import 9 temporary pictures as placeholders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import cat1 from './assets/cat1.jpg';
+import cat2 from './assets/cat2.jpg';
+import cat3 from './assets/cat3.jpg';
+import cat4 from './assets/cat4.jpg';
+import cat5 from './assets/cat5.png';
+import cat6 from './assets/cat6.png';
+import cat7 from './assets/cat7.jpeg';
+import cat8 from './assets/cat8.jpeg';
+import cat9 from './assets/cat9.jpg';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>This Setup Works</h1>
-                <div className="container">
-                    <p>Yess!</p>
-                </div>
+                <h1>NASA Gallery</h1>
+                <section className="gallery">
+                    <img src={cat1} alt="" />
+                    <img src={cat2} alt="" />
+                    <img src={cat3} alt="" />
+                    <img src={cat4} alt="" />
+                    <img src={cat5} alt="" />
+                    <img src={cat6} alt="" />
+                    <img src={cat7} alt="" />
+                    <img src={cat8} alt="" />
+                    <img src={cat9} alt="" />
+                </section>
             </div>
         );
     }

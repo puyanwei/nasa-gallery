ded006e5cd2023416b25fd0d37bd0acbd9fabeee
Puyan Wei <puyanwei@gmail.com>
2018-07-19 10:18:47 +0100

    Add placeholder for image titles, adjust css spacing

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import './Image.css';
 
 class Image extends Component {
-	render() {
-		return <img src={this.props.link} alt="" />;
-	}
+    render() {
+        return (
+            <div className="image-wrapper">
+                <p>Title Text</p>
+                <img src={this.props.link} alt="" />
+            </div>
+        );
+    }
 }
 
 export default Image;
